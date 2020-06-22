8e3f113d10b46ec17fd0bb295c92369b3d222760
sarthakTUM <sarthak.gupta@tum.de>
2020-06-22 12:08:09 +0200

    requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from distutils.core import setup
 
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
 setup(
     name='fnp',
     version='0.0.1',
-    packages=[],
     license='',
     long_description=open('README.txt').read(),
+    packages=find_packages(exclude="tests"),  # same as name
+    install_requires=required,
+    include_package_data=True,
+    python_requires=">=3.6",
 )
